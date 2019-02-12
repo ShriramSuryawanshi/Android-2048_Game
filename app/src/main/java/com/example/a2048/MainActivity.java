b3568d26584dac49385def5b845368d98c23c7b7
ShriramSuryawanshi <shriram.a.suryawanshi@gmail.com>
2019-02-12 18:38:06 -0500

    more transition animation

--- app/src/main/java/com/example/a2048/MainActivity.java
+++ app/src/main/java/com/example/a2048/MainActivity.java
@@ -6,6 +6,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.TranslateAnimation;
 import android.widget.TextView;
 import android.view.MotionEvent;
 import java.util.Random;
@@ -17,21 +19,27 @@ public class MainActivity extends AppCompatActivity  {
     private GestureDetectorCompat gestureDetectorCompat = null;
     public boolean start = true;
     public int[] array = new int[4];
-    public String[] colorCodes = {"#e22a64", "#40E0D0", "#672ae2", "#2abde2", "#cfe22a", "#e29e2a", "#e2582a", "#2ae2d5", "#800000", "#008000", "#008000"};
+    public String[] colorCodes = {"#fffff0", "#cccc78", "#e8e812", "#e8e812", "#18e295", "#18d8e2", "#2e25d1", "#2ae2d5", "#800000", "#008000", "#008000"};
 
     public int[] addedWith = new int[4];
+    public int[] movedFrom = new int[4];
 
+    public int value;
+    public int n;
 
     public void formatBoard() {
 
         for(int i = 1; i <= 16; i++) {
             TextView textView = (TextView) findViewById(getResources().getIdentifier("textView" + i,"id", this.getPackageName()));
 
-            if (textView.getText().toString().equalsIgnoreCase(""))
+            if (textView.getText().toString().equalsIgnoreCase("")) {
                 textView.setBackgroundColor(Color.parseColor("#FFFAFA"));
+                textView.animate().alpha(0.2f).setDuration(50);
+            }
             else {
                 textView.setBackgroundColor(Color.parseColor(colorCodes[(int) (Math.log(Integer.parseInt(textView.getText().toString())) / Math.log(2))]));
                 textView.setTextColor(Color.parseColor("#000000"));
+                textView.animate().alpha(1f).setDuration(50);
             }
         }
     }
@@ -49,12 +57,15 @@ public class MainActivity extends AppCompatActivity  {
             if(start) {
                 textView.setText("2");
                 textView.setTextColor(Color.parseColor("#000000"));
+                textView.animate().alpha(1f).setDuration(50);
                 start = false;
                 newNumber();
             }
             else
                 textView.setText("2");
                 textView.setTextColor(Color.parseColor("#000000"));
+                textView.setBackgroundColor(Color.parseColor("#FFFAFA"));
+                textView.animate().alpha(1f).setDuration(50);
         }
         else
             newNumber();
@@ -77,7 +88,7 @@ public class MainActivity extends AppCompatActivity  {
                 else if (array[a] == array[b]) {
                     array[a] = array[a] + array[b];
                     array[b] = 0;
-                    addedWith[cnt++] = b;
+                    addedWith[a] = b;
                     break;
                 } else
                     break;
@@ -86,11 +97,13 @@ public class MainActivity extends AppCompatActivity  {
     }
 
 
-    public void moveElement() {
+    public void moveElements() {
 
         int[] array1 = new int[4];
-        for(int a = 0; a <= 3; a++)
+        for(int a = 0; a <= 3; a++) {
             array1[a] = 0;
+            movedFrom[a] = 0;
+        }
 
         int cnt = 0;
 
@@ -98,12 +111,43 @@ public class MainActivity extends AppCompatActivity  {
 
             if(array[a] != 0) {
                 array1[cnt] = array[a];
+                movedFrom[cnt] = a;
                 cnt++;
             }
         }
 
         for(int a = 0; a <= 3; a++)
             array[a] = array1[a];
+    }
+
+
+    public void animationTransition(final TextView animationTextView, final int local_n, final int local_value, int fromxDelta, int toXDelta, int fromYDelta, int toYDelta) {
+
+        Log.i("AtStart", String.valueOf(local_value) + " to be set at " + String.valueOf(local_n));
+        Animation animation = new TranslateAnimation(fromxDelta, toXDelta, fromYDelta, toYDelta);
+        animation.setDuration(200);
+        animation.setFillAfter(false);
+        animation.setAnimationListener(new Animation.AnimationListener(){
+            @Override public void onAnimationStart(Animation animation){}
+            @Override public void onAnimationRepeat(Animation animation){}
+            @Override public void onAnimationEnd(Animation animation){
+                Log.i("AtEnd", String.valueOf(local_value) + " set at " + String.valueOf(local_n));
+                TextView textView = (TextView) findViewById(getResources().getIdentifier("textView" + local_n, "id", getPackageName()));
+                textView.setText(local_value == 0 ? "" : String.valueOf(local_value));
+                formatBoard();
+            }
+        });
+
+        animationTextView.startAnimation(animation);
+        animationTextView.postDelayed(new Runnable() { @Override public void run() { }}, 200);
+        animationTextView.setText("");
+        animationTextView.animate().alpha(0.2f).setDuration(0);
+    }
+
+
+    public void fadeAnimation(TextView animationTextView, int textViewID, int textValue) {
+
+
     }
 
 
@@ -114,52 +158,44 @@ public class MainActivity extends AppCompatActivity  {
             TextView textView1 = (TextView) findViewById(getResources().getIdentifier("textView" + i, "id", this.getPackageName()));
             array[0] = textView1.getText().toString().equalsIgnoreCase("") ? 0 : Integer.parseInt(textView1.getText().toString());
 
-            TextView textView2 = (TextView) findViewById(getResources().getIdentifier("textView" + (i+1), "id", this.getPackageName()));
+            TextView textView2 = (TextView) findViewById(getResources().getIdentifier("textView" + (i + 1), "id", this.getPackageName()));
             array[1] = textView2.getText().toString().equalsIgnoreCase("") ? 0 : Integer.parseInt(textView2.getText().toString());
 
-            TextView textView3 = (TextView) findViewById(getResources().getIdentifier("textView" + (i+2), "id", this.getPackageName()));
+            TextView textView3 = (TextView) findViewById(getResources().getIdentifier("textView" + (i + 2), "id", this.getPackageName()));
             array[2] = textView3.getText().toString().equalsIgnoreCase("") ? 0 : Integer.parseInt(textView3.getText().toString());
 
-            TextView textView4 = (TextView) findViewById(getResources().getIdentifier("textView" + (i+3), "id", this.getPackageName()));
+            TextView textView4 = (TextView) findViewById(getResources().getIdentifier("textView" + (i + 3), "id", this.getPackageName()));
             array[3] = textView4.getText().toString().equalsIgnoreCase("") ? 0 : Integer.parseInt(textView4.getText().toString());
 
             sumElements();
-           // Log.i("addedElements", String.valueOf(addedWith[0]) + ", " + String.valueOf(addedWith[1]) + ", " + String.valueOf(addedWith[2]) + ", " + String.valueOf(addedWith[3]));
 
-            if(addedWith[0] == 1) {
-                textView2.animate().translationX(-250f).setDuration(500);
-                textView2.setTranslationX(250f);
-            } else if (addedWith[0] == 2) {
-                textView3.animate().translationX(-500f).setDuration(500);
-                textView3.setTranslationX(500f);
-            } else if (addedWith[0] == 3) {
-                textView4.animate().translationX(-750f).setDuration(500);
-                textView4.setTranslationX(750f);
-            }
-            textView1.setText(array[0] == 0 ? "" : String.valueOf(array[0]));
+            if(addedWith[0] == 1)               animationTransition(textView2,i+0, array[0],0, -250, 0, 0);
+            else if (addedWith[0] == 2)         animationTransition(textView3,i+0, array[0],0, -500, 0, 0);
+            else if (addedWith[0] == 3)         animationTransition(textView4,i+0, array[0],0, -750, 0, 0);
 
-            if(addedWith[1] == 2) {
-                textView3.animate().translationX(-250f).setDuration(500);
-                textView3.setTranslationX(250f);
-            } else if (addedWith[1] == 3) {
-                textView4.animate().translationX(-500f).setDuration(500);
-                textView4.setTranslationX(500f);
-            }
-            textView2.setText(array[1] == 0 ? "" : String.valueOf(array[1]));
+            if(addedWith[1] == 2)               animationTransition(textView3,i+1, array[1],0, -250, 0, 0);
+            else if (addedWith[1] == 3)         animationTransition(textView4,i+1, array[1],0, -500, 0, 0);
 
-            if(addedWith[2] == 3) {
-                textView4.animate().translationX(-250f).setDuration(500);
-                textView4.setTranslationX(250f);
-            }
+            if(addedWith[2] == 3)               animationTransition(textView4,i+2, array[2],0, -250, 0, 0);
 
-            textView3.setText(array[2] == 0 ? "" : String.valueOf(array[2]));
             textView4.setText(array[3] == 0 ? "" : String.valueOf(array[3]));
+
+            moveElements();
+
+            if(movedFrom[0] == 1)               animationTransition(textView2,i+0, array[0],0, -250, 0, 0);
+            else if (movedFrom[0] == 2)         animationTransition(textView3,i+0, array[0],0, -500, 0, 0);
+            else if (movedFrom[0] == 3)         animationTransition(textView4,i+0, array[0],0, -750, 0, 0);
+
+            if(movedFrom[1] == 2)               animationTransition(textView3,i+1, array[1],0, -250, 0, 0);
+            else if (movedFrom[1] == 3)         animationTransition(textView4,i+1, array[1],0, -500, 0, 0);
+
+            if(movedFrom[2] == 3)               animationTransition(textView4,i+2, array[2],0, -250, 0, 0);
+
+            textView4.setText(array[3] == 0 ? "" : String.valueOf(array[3]));
+
         }
 
-        formatBoard();
         newNumber();
-
-
     }
 
 
@@ -180,6 +216,7 @@ public class MainActivity extends AppCompatActivity  {
             array[0] = textView4.getText().toString().equalsIgnoreCase("") ? 0 : Integer.parseInt(textView4.getText().toString());
 
             sumElements();
+            moveElements();
 
             textView1.setText(array[3] == 0 ? "" : String.valueOf(array[3]));
             textView2.setText(array[2] == 0 ? "" : String.valueOf(array[2]));
@@ -210,13 +247,42 @@ public class MainActivity extends AppCompatActivity  {
 
             sumElements();
 
+            if(addedWith[0] == 1)               animationTransition(textView2,i+0, array[0],0, 0, 0, -190);
+            else if (addedWith[0] == 2)         animationTransition(textView3,i+0, array[0],0, 0, 0, -380);
+            else if (addedWith[0] == 3)         animationTransition(textView4,i+0, array[0],0, 0, 0, -570);
+
+            if(addedWith[1] == 2)               animationTransition(textView3,i+4, array[1],0, 0, 0, -190);
+            else if (addedWith[1] == 3)         animationTransition(textView4,i+4, array[1],0, 0, 0, -380);
+
+            if(addedWith[2] == 3)               animationTransition(textView4,i+8, array[2],0, 0, 0, -570);
+
+            textView4.setText(array[3] == 0 ? "" : String.valueOf(array[3]));
+
+            moveElements();
+
+            if(movedFrom[0] == 1)               animationTransition(textView2,i+0, array[0],0, 0, 0, -190);
+            else if (movedFrom[0] == 2)         animationTransition(textView3,i+0, array[0],0, 0, 0, -380);
+            else if (movedFrom[0] == 3)         animationTransition(textView4,i+0, array[0],0, 0, 0, -570);
+
+            if(movedFrom[1] == 2)               animationTransition(textView3,i+4, array[1],0, 0, 0, -190);
+            else if (movedFrom[1] == 3)         animationTransition(textView4,i+4, array[1],0, 0, 0, -380);
+
+            if(movedFrom[2] == 3)               animationTransition(textView4,i+8, array[2],0, 0, 0, -380);
+
+            textView4.setText(array[3] == 0 ? "" : String.valueOf(array[3]));
+
+
+
+
+          /*  moveElements();
+
             textView1.setText(array[0] == 0 ? "" : String.valueOf(array[0]));
             textView2.setText(array[1] == 0 ? "" : String.valueOf(array[1]));
             textView3.setText(array[2] == 0 ? "" : String.valueOf(array[2]));
-            textView4.setText(array[3] == 0 ? "" : String.valueOf(array[3]));
+            textView4.setText(array[3] == 0 ? "" : String.valueOf(array[3])); */
         }
 
-        formatBoard();
+        //formatBoard();
         newNumber();
     }
 
@@ -238,6 +304,7 @@ public class MainActivity extends AppCompatActivity  {
             array[0] = textView4.getText().toString().equalsIgnoreCase("") ? 0 : Integer.parseInt(textView4.getText().toString());
 
             sumElements();
+            moveElements();
 
             textView1.setText(array[3] == 0 ? "" : String.valueOf(array[3]));
             textView2.setText(array[2] == 0 ? "" : String.valueOf(array[2]));
@@ -263,7 +330,7 @@ public class MainActivity extends AppCompatActivity  {
 
             TextView textView = (TextView) findViewById(getResources().getIdentifier("textView" + i,"id", this.getPackageName()));
             textView.setText("");
-            textView.setBackgroundColor(Color.parseColor("#FFFAFA"));
+            textView.animate().alpha(0.2f).setDuration(0);
         }
 
         newNumber();
